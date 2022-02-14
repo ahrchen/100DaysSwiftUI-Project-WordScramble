63d593b445111a6e6f539ce62891123b65d11320
Raymond Chen <ahrchen@gmail.com>
2022-02-13 20:56:11 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Raymond Chen on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
